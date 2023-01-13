3d8e12da1a57ad015fddc9a379d49d1f49d55bc6
Kevin McDonnell <44483116+kevintmcdonnell@users.noreply.github.com>
2023-01-13 11:14:22 -0500

    Fixed typo

--- tests.c
+++ tests.c
@@ -5,7 +5,7 @@
 
 #define TEST_TIMEOUT 3 
 
-Test(base_suit, sample_test, .timeout=TEST_TIMEOUT) {
+Test(base_suite, sample_test, .timeout=TEST_TIMEOUT) {
     int ret = compute(2);
     cr_assert_eq(8, ret, 
         "Return value should have been %d, but it was %d.\n", 8, ret);
